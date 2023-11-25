c3eee3e6910a1c4822a1be96df7fac914f72e4af
yasin2514 <yasinkhanrabbi1999@gmail.com>
2023-11-25 07:13:05 +0600

    user interface added

---
+++ src/modules/users/user.interface.ts
@@ -0,0 +1,30 @@
+// ------------------------sub interface----------------------------
+export type TUserName = {
+  firstName: string;
+  lastName: string;
+};
+
+export type TAddress = {
+  street: string;
+  city: string;
+  country: string;
+};
+
+export type TOrders = {
+  productName: string;
+  price: number;
+  quantity: number;
+};
+
+// ----------------------main interface-----------------------
+export type TUser = {
+  userId: number;
+  username: string;
+  password: string;
+  fullName: TUserName;
+  age: number;
+  email: string;
+  isActive: boolean;
+  address: TAddress;
+  orders: TOrders;
+};
